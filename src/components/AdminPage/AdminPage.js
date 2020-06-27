9015e8dec0e9273cb53c4ad8af82d7f625bb3e07
thanna96 <thanna96@gmail.com>
2020-06-27 15:10:45 -0400

    Fix .env variables

--- src/components/AdminPage/AdminPage.js
+++ src/components/AdminPage/AdminPage.js
@@ -8,8 +8,8 @@ const AWS = require("aws-sdk");
 
 AWS.config.update({
     region: "us-east-1",
-    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
+    accessKeyId: "g",
+    secretAccessKey: "t"
 });
 const docClient = new AWS.DynamoDB.DocumentClient();
 
