e3ec1ed4a3aed46815e75fef1039c4481d35aebd
thanna96 <thanna96@gmail.com>
2020-07-15 16:42:24 -0400

    spelling caused error

--- src/components/Header/MobileNav.js
+++ src/components/Header/MobileNav.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Navbar from "react-bootstrap/Navbar";
-import Nav from 'react-bootstrap/nav'
+import Nav from 'react-bootstrap/Nav'
 import NavDropdown from "react-bootstrap/NavDropdown";
 import {Link} from "react-router-dom";
 import Button from "react-bootstrap/Button"
